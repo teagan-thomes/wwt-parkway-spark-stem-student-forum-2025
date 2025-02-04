f0b1172dd9151b72f690b700c7f76442459eb789
Teagan Thomes <teagan@anydesk.net>
2025-02-04 13:04:43 -0600

    header correctly sits at the bottom of the page, create styles.css

--- website/imports/loadImports.js
+++ website/imports/loadImports.js
@@ -2,12 +2,12 @@ fetch("/website/imports/header.html")
 	.then((response) => response.text())
 	.then((data) => {
 		document.getElementById("header-placeholder").innerHTML = data;
+		console.log("Header loaded");
 	});
 
 fetch("/website/imports/footer.html")
 	.then((response) => response.text())
 	.then((data) => {
 		document.getElementById("footer-placeholder").innerHTML = data;
+		console.log("Footer loaded");
 	});
-
-console.log("Imports loaded");
